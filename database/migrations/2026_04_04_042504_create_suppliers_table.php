f66af9491cbc50393e61f8c99d856bd673aece31
Lanang-123 <lanangobey123@gmail.com>
2026-04-04 13:07:57 +0800

    buat migrasi v2

--- database/migrations/2026_04_04_042504_create_suppliers_table.php
+++ database/migrations/2026_04_04_042504_create_suppliers_table.php
@@ -10,12 +10,20 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('suppliers', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('suppliers', function (Blueprint $table) {
+        $table->id();
+
+        $table->string('name'); // Nama Perusahaan/Toko Supplierr
+        $table->string('contact_person')->nullable(); // Nama sales/orang yang bisa dihubungi
+        $table->string('phone', 20)->nullable(); // Nomor WA/Telepon
+        $table->text('address')->nullable(); // Alamat untuk retur barang
+        $table->string('email')->nullable();
+        $table->boolean('is_active')->default(true); // Soft hapus, agar data tidak benar-benar hilang jika ada transaksi terikat
+
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
